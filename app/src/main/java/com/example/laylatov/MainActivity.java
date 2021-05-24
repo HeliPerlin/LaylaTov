13be1f006d65b44d557ff7080d6eb96f34488de6
HeliPerlin <heliperlin@gmail.com>
2021-05-24 15:26:14 +0300

    added welcome screen, pdf screen and choosing screen

--- app/src/main/java/com/example/laylatov/MainActivity.java
+++ app/src/main/java/com/example/laylatov/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.laylatov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
+        Button moveToInfoBtn = (Button) findViewById(R.id.button);
+
+        moveToInfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToInfoScreen();
+            }
+        });
     }
+
+
+        public void moveToInfoScreen(){
+            Intent intent = new Intent(this, WelcomeScreen.class);
+            startActivity(intent);
+        }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/laylatov/WelcomeScreen.java
@@ -0,0 +1,47 @@
+package com.example.laylatov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        Button findBarsBtn = (Button) findViewById(R.id.findBars);
+
+        Button goToInfoBtn = (Button) findViewById(R.id.goToInfo);
+
+        findBarsBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                moveToInfoScreen();
+            }
+        });
+
+        goToInfoBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                moveToFindBarsScreen();
+            }
+        });
+    }
+
+    public void moveToInfoScreen(){
+        Intent intent = new Intent(this, UsefulInfoClient.class);
+        startActivity(intent);
+    }
+
+    public void moveToFindBarsScreen(){
+        Intent intent = new Intent(this, UsefulInfoClient.class); // TODO: ADD SCREEN
+        startActivity(intent);
+    }
+
+}
